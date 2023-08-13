cf605c56ca26c7b072a0d37604aa2a250bda9d45
Trapsito <49081110+Trapsito@users.noreply.github.com>
2023-08-12 23:03:55 -0400

    Creation Notification

---
+++ Patrones Creacionales/Singleton/Notificactions.ts
@@ -0,0 +1,20 @@
+class NotificationService {
+  private static instance: NotificationService
+
+  private constructor() {}
+
+  public static getInstance() {
+    if (!NotificationService.instance) {
+      NotificationService.instance = new NotificationService()
+    }
+
+    return NotificationService.instance
+  }
+
+  public sendNotification(message: string): string {
+    return `notificación enviada, mensaje: ${message}`
+  }
+}
+
+const notificationServise = NotificationService.getInstance()
+notificationServise.sendNotification('Notificación de prueba!')
